9cec9acd5945f915c110b5e6cbb6b3e3c05e9bc9
jinxingjiax <jin.xingjiax@99cloud.net>
2020-12-02 23:21:01 +0800

    github config

--- babyspace-config/src/main/java/com/jyinit/babyspace/config/ConfigApplication.java
+++ babyspace-config/src/main/java/com/jyinit/babyspace/config/ConfigApplication.java
@@ -2,6 +2,7 @@ package com.jyinit.babyspace.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
@@ -9,6 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @SpringBootApplication
 @EnableEurekaClient
+@EnableConfigServer
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
